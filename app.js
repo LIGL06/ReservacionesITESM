050b70d4ce8c2f761724d86aabcf96052a782de7
Luis Iván García Luna <Luis Ivn Garca Luna>
2015-12-14 13:24:11 -0600

    Error con la Key.properties
    Al perecer la API de Stormpath necesita que la API Key se llame
    apiKey.properties

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
+'use strict'
 /*Dependencias de la Main API*/
 const bodyParser = require ('body-parser')
 const express = require ('express')
 const stormpath = require ('express-stormpath')
 const method_override = require('method-override')
 const mongoose = require('mongoose')
-const multer = require('multer')
+// const multer = require('multer')
 const app = express()
-const mupload = multer({dest:'uploads/'})
-const APIfile = __dirname + '/Key.properties'
-const APIid = '6AI4RE7GB8HNBGPOVAA8D3BOF'
-const APIsecret = 'Ki6zk4DNuTs/+cky2ProC4szDeZWoMJiO0vIRW5YhaI'
-const APIpath = 'https://api.stormpath.com/v1/applications/1j3A4OUqTuPikLGcU5M7Yy'
+// const mupload = multer({dest:'uploads/'})
+// const APIfile = __dirname + '/Key.properties'
+// const APIid = '6AI4RE7GB8HNBGPOVAA8D3BOF'
+// const APIsecret = 'Ki6zk4DNuTs/+cky2ProC4szDeZWoMJiO0vIRW5YhaI'
+// const APIpath = 'https://api.stormpath.com/v1/applications/6dE9P78g0bbttctVm5dOjo'
 const Schema = mongoose.Schema
 
 /*Variables de sistema*/
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 12000
 
 mongoose.connect('mongodb://localhost/itesm',function(error,db){
   if(!error) console.log('Conexión a DB')
@@ -27,33 +28,10 @@ app.use(express.static('public'))
 /*Auth*/
 /*Stormpath Auth Init*/
 app.use(stormpath.init(app,{
-  "client": {
-    "apiKey":{
-      "file": APIfile,
-      "id": APIid,
-      "secret": APIsecret
-    },
-      "cacheManager":{
-        "defaultTtl": 300,
-        "defaultTti": 300,
-        "caches": {
-          "account": {
-            "ttl": 300,
-            "tti": 300
-          }
-        }
-      },
-  "baseUrl": "https://api.stormpath.com/v1",
-  "connectionTimeout": 30,
-  "authenticationScheme": "SAUTHC1", // or "BASIC"
-  "proxy":{
-    "port": "",
-    "host": "",
-    "username": "",
-    "password": ""
-  }
-},
-  website: true,
+  application:{
+    href: 'https://api.stormpath.com/v1/applications/6dE9P78g0bbttctVm5dOjo'
+  },
+  website: true
 }))
 
 
